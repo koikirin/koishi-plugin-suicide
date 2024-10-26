2bc826ab21abdf6c3440f4874185574cb4f1424c
Hieuzest <girkirin@hotmail.com>
2024-10-27 00:22:14 +0800

    feat: intro shutdown service and event

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,57 @@
-import { Context, Schema } from 'koishi'
+import { Awaitable, Context, Schema, Service } from 'koishi'
 import { } from '@koishijs/plugin-notifier'
 
-export const name = 'suicide'
+declare module 'koishi' {
+  interface Events {
+    'shutdown'(): Awaitable<void>
+  }
 
-export const inject = ['notifier']
-
-export interface Config {}
-
-export const Config: Schema<Config> = Schema.object({})
-
-export function apply(ctx: Context) {
-  const suicide = () => ctx.loader.fullReload()
-
-  ctx.notifier.create({
-    type: 'danger',
-    content: <><p><button onClick={suicide}>RESTART</button></p></>,
-  })
-
-  ctx.command('suicide', { authority: 4 }).action(() => suicide())
+  interface Context {
+    shutdown: ShutdownService
+  }
 }
+
+export interface ShutdownService {
+  (): Awaitable<void>
+}
+
+export class ShutdownService extends Service {
+  constructor(ctx: Context) {
+    super(ctx, 'shutdown')
+
+    const suicide = () => ctx.loader.fullReload()
+    const shutdown = () => this()
+
+    ctx.notifier.create({
+      type: 'danger',
+      content: <><p><button onClick={suicide}>RESTART</button></p></>,
+    })
+
+    ctx.notifier.create({
+      type: 'danger',
+      content: <><p><button onClick={shutdown}>SHUTDOWN</button></p></>,
+    })
+
+    ctx.command('suicide', { authority: 4 }).action(suicide)
+    ctx.command('suicide.shutdown', { authority: 4 }).action(shutdown)
+  }
+
+  async notify() {
+    return this.ctx.parallel('shutdown')
+  }
+
+  async [Service.invoke]() {
+    await this.notify()
+    this.ctx.get('loader').fullReload()
+  }
+}
+
+export namespace ShutdownService {
+  export const inject = ['notifier']
+
+  export interface Config {}
+
+  export const Config: Schema<Config> = Schema.object({})
+}
+
+export default ShutdownService
